7bc198b3cbca7dc34a43d3d37846b2e1e3e4264c
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-12 22:19:23 +0100

    initial commit

---
+++ src/test/java/com/pawelapps/wiki/TestWikiApplication.java
@@ -0,0 +1,23 @@
+package com.pawelapps.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestWikiApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(WikiApplication::main).with(TestWikiApplication.class).run(args);
+    }
+
+}
