2a54aef018a896dc8339150c01500aa34322a9f1
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-15 23:38:24 +0100

    feat: create constructor and get methods

--- src/main/java/com/pawelapps/wiki/program/ProgramController.java
+++ src/main/java/com/pawelapps/wiki/program/ProgramController.java
@@ -1,7 +1,37 @@
 package com.pawelapps.wiki.program;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class ProgramController {
+
+    private final ProgramService programService;
+
+    @Autowired
+    public ProgramController(ProgramService programService){
+        this.programService = programService;
+    }
+
+    @GetMapping("/programs")
+    public ResponseEntity<List<Program>> getAllPrograms() {
+        List<Program> programs = programService.getAllPrograms();
+        return new ResponseEntity<>(programs, HttpStatus.OK);
+    }
+
+    @GetMapping("programs/{id}")
+    public ResponseEntity<Program> getProgramById(@PathVariable("id") Long id){
+        Program program = programService.getProgramById(id);
+        return new ResponseEntity<>(program, HttpStatus.OK);
+    }
+
 }
