2aac627f3065f07ed2fa10a4a0f151d87ba10252
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-15 22:35:22 +0100

    fix: change column name

--- src/main/java/com/pawelapps/wiki/user/User.java
+++ src/main/java/com/pawelapps/wiki/user/User.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Getter
 @Builder
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
