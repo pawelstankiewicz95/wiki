1a127cad49fc68384d9875c25570b0a790f6cd7f
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-01-15 15:42:03 +0100

    feat: add getAllPrograms method, add constructor

--- src/main/java/com/pawelapps/wiki/program/ProgramServiceImpl.java
+++ src/main/java/com/pawelapps/wiki/program/ProgramServiceImpl.java
@@ -1,7 +1,24 @@
 package com.pawelapps.wiki.program;
 
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@Transactional
 public class ProgramServiceImpl implements ProgramService{
+
+    private final ProgramRepository programRepository;
+
+    @Autowired
+    public ProgramServiceImpl(ProgramRepository programRepository) {
+        this.programRepository = programRepository;
+    }
+
+    @Override
+    public List<Program> getAllPrograms() {
+        return programRepository.findAll();
+    }
 }
