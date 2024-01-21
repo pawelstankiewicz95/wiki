295b72348f9fa46dd33cdf7b7810a34bd3aafc70
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-21 22:01:26 +0100

    feat: add security config

---
+++ src/main/java/com/pawelapps/wiki/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.pawelapps.wiki.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((request) -> request
+                        .anyRequest().permitAll()
+                );
+        httpSecurity.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+        return httpSecurity.build();
+    }
+
+}
