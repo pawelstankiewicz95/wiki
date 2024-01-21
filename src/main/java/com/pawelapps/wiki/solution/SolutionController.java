be719f892a037ab6c959fea50381f96ff137eecc
pawelstankiewicz <pawelsta95@gmail.com>
2024-01-21 13:13:55 +0100

    feat: add getSolutionBySubjectId method

--- src/main/java/com/pawelapps/wiki/solution/SolutionController.java
+++ src/main/java/com/pawelapps/wiki/solution/SolutionController.java
@@ -1,7 +1,28 @@
 package com.pawelapps.wiki.solution;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class SolutionController {
+    private final SolutionService solutionService;
+
+    @Autowired
+    public SolutionController(SolutionService solutionService) {
+        this.solutionService = solutionService;
+    }
+
+    @GetMapping("/solutions-by-subject-id/{subjectId}")
+    public ResponseEntity<List<Solution>> getSolutionBySubjectId(@PathVariable("subjectId") Long id){
+        List<Solution> solutions = solutionService.findBySubjectId(id);
+        return new ResponseEntity<>(solutions, HttpStatus.OK);
+    }
 }
