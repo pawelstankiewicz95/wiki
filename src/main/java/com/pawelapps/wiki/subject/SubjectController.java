560c48420745e4e7243db427ee53e9eca7cf272e
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-01-22 15:39:43 +0100

    feat: create getSubjectsByCategoryId endpoint

--- src/main/java/com/pawelapps/wiki/subject/SubjectController.java
+++ src/main/java/com/pawelapps/wiki/subject/SubjectController.java
@@ -1,7 +1,30 @@
 package com.pawelapps.wiki.subject;
 
-import org.springframework.stereotype.Controller;
+import com.pawelapps.wiki.category.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    @Autowired
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping("subjects/subjects-by-program-id/{subjectId}")
+    public ResponseEntity<List<Subject>> getSubjectsByCategoryId(@PathVariable("subjectId") Long categoryId){
+        List<Subject> subjects = subjectService.findByCategoryId(categoryId);
+        return new ResponseEntity<>(subjects, HttpStatus.OK);
+    }
 }
