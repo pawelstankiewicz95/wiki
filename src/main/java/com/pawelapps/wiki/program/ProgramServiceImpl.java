c6be2ce8e032abdc5f4242ce1473bbfcf1c005c0
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-01-30 10:09:17 +0100

    feat: add transactional annotations

--- src/main/java/com/pawelapps/wiki/program/ProgramServiceImpl.java
+++ src/main/java/com/pawelapps/wiki/program/ProgramServiceImpl.java
@@ -1,8 +1,8 @@
 package com.pawelapps.wiki.program;
 
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
