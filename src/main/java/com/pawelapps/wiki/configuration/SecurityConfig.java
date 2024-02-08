8c03dfd9076d9332f792155d9fa9341a96387cdb
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-02-08 09:32:18 +0100

    feat: add corsConfigurationSource bean

--- src/main/java/com/pawelapps/wiki/configuration/SecurityConfig.java
+++ src/main/java/com/pawelapps/wiki/configuration/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -24,10 +29,14 @@ public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
 
+    private final CorsConfigurationSource corsConfigurationSource;
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests(req ->
                         req.requestMatchers("/api/auth/**").anonymous()
                                 .requestMatchers(HttpMethod.GET).anonymous()
@@ -36,20 +45,18 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-        // .logout(logout ->
-        //  logout.logoutUrl("/api/v1/auth/logout")
-        //     .addLogoutHandler(logoutHandler)
-        //       .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
-        //)
-        ;
-
-
-        // httpSecurity.authorizeHttpRequests((request) -> request
-        //          .anyRequest().permitAll()
-        //    );
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         httpSecurity.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
         return httpSecurity.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
