1ee2711b51fc33b8e24c0e88e07b8954ba2aacc2
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-09 22:57:39 +0100

    feat:add find by title and by id methods

--- src/main/java/com/pawelapps/wiki/subject/SubjectService.java
+++ src/main/java/com/pawelapps/wiki/subject/SubjectService.java
@@ -4,4 +4,8 @@ import java.util.List;
 
 public interface SubjectService {
     List<Subject> findByCategoryId(Long id);
+
+    List<Subject> findByTitle(String title);
+
+    Subject findById(Long id);
 }
