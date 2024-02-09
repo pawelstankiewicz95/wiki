c1acce431cc6b29df69164db13f5bfc8b4827e61
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-09 23:00:39 +0100

    feat: add findById, save, update, delete methods

--- src/main/java/com/pawelapps/wiki/solution/SolutionService.java
+++ src/main/java/com/pawelapps/wiki/solution/SolutionService.java
@@ -4,5 +4,13 @@ import java.util.List;
 
 public interface SolutionService {
 
+    Solution findById(Long id);
+
     List<Solution> findBySubjectId(Long id);
+
+    Solution saveSolution(Long subjectId, Solution solution);
+
+    Solution updateSolution(Solution solution);
+
+    void deleteSolution(Long id);
 }
