69f3faefc01a81a97874241527f9596e480dc950
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-09 22:58:51 +0100

    feat:add search-by-title endpoint

--- src/main/java/com/pawelapps/wiki/subject/SubjectController.java
+++ src/main/java/com/pawelapps/wiki/subject/SubjectController.java
@@ -4,10 +4,7 @@ import com.pawelapps.wiki.category.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,12 @@ public class SubjectController {
     @GetMapping("subjects/subjects-by-category-id/{subjectId}")
     public ResponseEntity<List<Subject>> getSubjectsByCategoryId(@PathVariable("subjectId") Long categoryId){
         List<Subject> subjects = subjectService.findByCategoryId(categoryId);
+        return new ResponseEntity<>(subjects, HttpStatus.OK);
+    }
+
+    @GetMapping("subjects/search-by-title")
+    public ResponseEntity<List<Subject>> getSubjectsByTitle(@RequestParam("title") String title){
+        List<Subject> subjects = subjectService.findByTitle(title);
         return new ResponseEntity<>(subjects, HttpStatus.OK);
     }
 }
