484311f92d347a5c28d28fc1bdcef4eaa725a606
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-17 00:04:12 +0100

    add user handling, create dtos

---
+++ src/main/java/com/pawelapps/wiki/solution/SolutionResponse.java
@@ -0,0 +1,21 @@
+package com.pawelapps.wiki.solution;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.pawelapps.wiki.subject.SubjectResponse;
+import com.pawelapps.wiki.user.UserResponse;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record SolutionResponse(Long id,
+                               @JsonProperty("user")
+                               UserResponse userResponse,
+                               @JsonIgnoreProperties({"userResponse"})
+                               SubjectResponse subjectResponse,
+                               String description,
+                               LocalDateTime timeCreated,
+                               LocalDateTime timeUpdated) {
+}
+
