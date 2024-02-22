716f1e752d2f87fb7526db9c356b6de64b3cb379
pawelstankiewicz <pawelsta95@gmail.com>
2024-02-22 21:28:18 +0100

    fix: add JpaRepository extension

--- src/main/java/com/pawelapps/wiki/solution/image/ImageRepository.java
+++ src/main/java/com/pawelapps/wiki/solution/image/ImageRepository.java
@@ -1,10 +1,11 @@
 package com.pawelapps.wiki.solution.image;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
 @Repository
-public interface ImageRepository {
+public interface ImageRepository extends JpaRepository<Image ,Long> {
     Set<Image> findBySolutionId(Long solutionId);
 }
