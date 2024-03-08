16de51da91c5f90dbfe8c2c7226752f42d43995b
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-03-08 15:05:15 +0100

    create getAnnouncements method

--- src/main/java/com/pawelapps/wiki/announcement/AnnouncementController.java
+++ src/main/java/com/pawelapps/wiki/announcement/AnnouncementController.java
@@ -1,8 +1,13 @@
 package com.pawelapps.wiki.announcement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api")
@@ -12,5 +17,11 @@ public class AnnouncementController {
     @Autowired
     public AnnouncementController(AnnouncementService announcementService) {
         this.announcementService = announcementService;
+    }
+
+    @GetMapping("/announcements")
+    public ResponseEntity<List<AnnouncementResponse>> getAnnouncements(){
+        List<AnnouncementResponse> announcements = announcementService.findAll();
+        return new ResponseEntity<>(announcements, HttpStatus.OK);
     }
 }
