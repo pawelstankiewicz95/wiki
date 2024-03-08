3d33fc18f4fce37e76b94f742545370513ab1c3f
pawelstankiewicz95 <pawelsta95@gmail.com>
2024-03-08 15:05:42 +0100

    add builder annotation

--- src/main/java/com/pawelapps/wiki/announcement/AnnouncementResponse.java
+++ src/main/java/com/pawelapps/wiki/announcement/AnnouncementResponse.java
@@ -2,7 +2,9 @@ package com.pawelapps.wiki.announcement;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pawelapps.wiki.user.UserResponse;
+import lombok.Builder;
 
+@Builder
 public record AnnouncementResponse(Long id,
                                    String title,
                                    String description,
