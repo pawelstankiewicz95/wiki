ffd83eba85797c45b2fa4b079c6e95ca5e117549
pawelstankiewicz <pawelsta95@gmail.com>
2024-03-11 23:24:34 +0100

    add saveMessage method

--- src/main/java/com/pawelapps/wiki/chat/message/MessageController.java
+++ src/main/java/com/pawelapps/wiki/chat/message/MessageController.java
@@ -5,8 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -23,6 +26,11 @@ public class MessageController {
     @GetMapping("/messages")
     public ResponseEntity<List<MessageDto>> getAllMessages() {
         return new ResponseEntity<>(messageService.getAllMessages(), HttpStatus.OK);
+    }
+
+    @PostMapping("/messages")
+    public ResponseEntity<MessageDto> saveMessage(@RequestBody MessageDto messageDto, Principal principal){
+        return new ResponseEntity<>(messageService.saveMessage(messageDto, principal.getName()),HttpStatus.CREATED);
     }
 
 }
