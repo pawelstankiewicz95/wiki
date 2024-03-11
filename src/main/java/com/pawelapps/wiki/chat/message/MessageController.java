c9d232de0a635b7b1fae4fe9bb8380e44b0cda52
pawelstankiewicz <pawelsta95@gmail.com>
2024-03-11 23:02:45 +0100

    feat: create message controller with getAllMessagesMethod

---
+++ src/main/java/com/pawelapps/wiki/chat/message/MessageController.java
@@ -0,0 +1,28 @@
+package com.pawelapps.wiki.chat.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/chat")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/messages")
+    public ResponseEntity<List<MessageDto>> getAllMessages() {
+        return new ResponseEntity<>(messageService.getAllMessages(), HttpStatus.OK);
+    }
+
+}
