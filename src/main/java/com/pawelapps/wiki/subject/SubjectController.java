e81a778f7d6c153dcd7e4f6d0527f8fd9e1395a8
pawelstankiewicz <pawelsta95@gmail.com>
2024-03-26 23:26:52 +0100

    add getSubjectById method

--- src/main/java/com/pawelapps/wiki/subject/SubjectController.java
+++ src/main/java/com/pawelapps/wiki/subject/SubjectController.java
@@ -1,6 +1,5 @@
 package com.pawelapps.wiki.subject;
 
-import com.pawelapps.wiki.solution.Solution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,19 +20,25 @@ public class SubjectController {
     }
 
     @GetMapping("subjects/subjects-by-category-id/{subjectId}")
-    public ResponseEntity<List<SubjectResponse>> getSubjectsByCategoryId(@PathVariable("subjectId") Long categoryId){
-        List<SubjectResponse> subjects = subjectService.findByCategoryId(categoryId);
+    public ResponseEntity<List<SubjectDto>> getSubjectsByCategoryId(@PathVariable("subjectId") Long categoryId) {
+        List<SubjectDto> subjects = subjectService.findByCategoryId(categoryId);
         return new ResponseEntity<>(subjects, HttpStatus.OK);
     }
 
     @GetMapping("subjects/search-by-title")
-    public ResponseEntity<List<SubjectResponse>> getSubjectsByTitle(@RequestParam("title") String title){
-        List<SubjectResponse> subjects = subjectService.findByTitle(title);
+    public ResponseEntity<List<SubjectDto>> getSubjectsByTitle(@RequestParam("title") String title) {
+        List<SubjectDto> subjects = subjectService.findByTitle(title);
         return new ResponseEntity<>(subjects, HttpStatus.OK);
     }
 
+    @GetMapping("subjects/{subjectId}")
+    public ResponseEntity<SubjectDto> getSubjectById(@PathVariable("subjectId") Long subjectId) {
+        SubjectDto subject = subjectService.getSubjectDto(subjectId);
+        return new ResponseEntity<>(subject, HttpStatus.OK);
+    }
+
     @PostMapping("/subjects")
-    public ResponseEntity<Subject> saveSubject(@RequestParam("categoryId") Long categoryId, Principal principal, @RequestBody Subject subject){
+    public ResponseEntity<Subject> saveSubject(@RequestParam("categoryId") Long categoryId, Principal principal, @RequestBody Subject subject) {
         Subject newSubject = this.subjectService.save(categoryId, principal.getName(), subject);
         return new ResponseEntity<>(newSubject, HttpStatus.CREATED);
     }
