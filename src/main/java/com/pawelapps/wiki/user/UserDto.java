abbfe118242d6777d171d6940a4ca75335c7c8f0
pawelstankiewicz <pawelsta95@gmail.com>
2024-04-16 23:04:38 +0200

    feat: add isEnabled field

--- src/main/java/com/pawelapps/wiki/user/UserDto.java
+++ src/main/java/com/pawelapps/wiki/user/UserDto.java
@@ -3,5 +3,5 @@ package com.pawelapps.wiki.user;
 import lombok.Builder;
 
 @Builder
-public record UserDto(String username, String email, String firstName, String lastName, Role role) {
+public record UserDto(String username, String email, String firstName, String lastName, Role role, Boolean isEnabled) {
 }
